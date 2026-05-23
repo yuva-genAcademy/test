e32f6b29a1a53f0e73aae3fc2997f07c5a24f4f4
yuva-genAcademy <yuva@thegenacademy.com>
2026-05-23 12:26:32 +0530

    Create test.py

---
+++ folder1/test.py
@@ -0,0 +1,11 @@
+numbers = [2, 4, 6, 8, 10]
+
+total = sum(numbers)
+average = total / len(numbers)
+
+print("Numbers:", numbers)
+print("Total:", total)
+print("Average:", average)
+
+for number in numbers:
+    print(number, "squared is", number ** 2)
